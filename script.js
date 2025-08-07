6516b38d9d869b01567162eab92e0e2c7bbd40b9
ero-sannin2 <rider.dragon320@gmail.com>
2025-08-07 14:43:00 +0545

    create functions displayBook and addBookToLibrary

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 const myLibrary=[];
+let output=document.querySelector("#output");
+
+
 function Book(id,title,author,pages,read_status){
     this.id=crypto.randomUUID();
     this.title=title;
     this.author=author;
     this.pages=pages;
-    this.read_status=read_status;
-    
+    this.read_status=read_status; 
 }
 
-function addBookToLibrary(name,pages,author){
-    tmp= new Book(name,ages,author);
+function addBookToLibrary(title,author,pages,read_status){
+    tmp= new Book(title,author,pages,read_status);
     myLibrary.append(tmp);
 }
 
 function displayBooks(){
     myLibrary.forEach((element)=>{
+        card=document.createElement("div");
+        card.setAttribute("class","card");
+        for(key in element){
+            card.innerText+=`${key}:${element[key]}\n`;
+        }
+
+        output.appendChild(card);
+    });
         
-    })
 }
+
+
+
